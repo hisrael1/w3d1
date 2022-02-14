d6e9edcb403ba0a1e5a58f22e46fe91b59706258
hisrael <harrisonisrael23@gmail.com>
2022-02-14 16:25:51 -0500

    test

--- enumerables.rb
+++ enumerables.rb
@@ -1,2 +1,17 @@
-p "hello"
-p "World"
+def my_each 
+end
+
+# calls my_each twice on the array, printing all the numbers twice.
+# return_value = [1, 2, 3].my_each do |num|
+#     puts num
+#    end.my_each do |num|
+#     puts num
+#    end
+#    # => 1
+#        2
+#        3
+#        1
+#        2
+#        3
+   
+#    p return_value  # => [1, 2, 3]
